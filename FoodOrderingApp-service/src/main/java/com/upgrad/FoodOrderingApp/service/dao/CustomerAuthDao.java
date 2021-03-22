3ca1530fd9cb4cc2706e5090d666495aef265c05
ChethanShetty563 <chethan563@gmail.com>
2021-03-23 00:48:36 +0530

    Added the Repository annotation

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/CustomerAuthDao.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/CustomerAuthDao.java
@@ -1,11 +1,14 @@
 package com.upgrad.FoodOrderingApp.service.dao;
 
 import com.upgrad.FoodOrderingApp.service.entity.CustomerEntity;
+import org.springframework.stereotype.Repository;
+
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 
+@Repository
 public class CustomerAuthDao {
 
     @PersistenceContext
