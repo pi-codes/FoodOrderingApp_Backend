4a8da04c05a6407fa4137101c362d1af69fbd403
ChethanShetty563 <chethan563@gmail.com>
2021-03-23 00:40:53 +0530

    Added the CustomerAuthEntity class

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/CustomerAuthEntity.java
@@ -0,0 +1,103 @@
+package com.upgrad.FoodOrderingApp.service.entity;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "CUSTOMER_AUTH")
+public class CustomerAuthEntity {
+
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "uuid")
+    @NotNull
+    @Size(max = 200)
+    private String uuid;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    @NotNull
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private CustomerEntity customer;
+
+    @Column(name = "access_token")
+    @Size(max = 500)
+    @NotNull
+    private String accessToken;
+
+    @Column(name = "expires_at")
+    @NotNull
+    private ZonedDateTime expiresAt;
+
+    @Column(name = "login_at")
+    @NotNull
+    private ZonedDateTime loginAt;
+
+    @Column(name = "logout_at")
+    private ZonedDateTime logoutAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public CustomerEntity getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(CustomerEntity customer) {
+        this.customer = customer;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public ZonedDateTime getExpiresAt() {
+        return expiresAt;
+    }
+
+    public void setExpiresAt(ZonedDateTime expiresAt) {
+        this.expiresAt = expiresAt;
+    }
+
+    public ZonedDateTime getLoginAt() {
+        return loginAt;
+    }
+
+    public void setLoginAt(ZonedDateTime loginAt) {
+        this.loginAt = loginAt;
+    }
+
+    public ZonedDateTime getLogoutAt() {
+        return logoutAt;
+    }
+
+    public void setLogoutAt(ZonedDateTime logoutAt) {
+        this.logoutAt = logoutAt;
+    }
+}
