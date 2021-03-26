92d7bbf4df1be77c49e45c5cd0e1575b872e8215
ChethanShetty563 <chethan563@gmail.com>
2021-03-26 09:41:55 +0530

    Added state dao class to access the state realted information

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/StateDao.java
@@ -0,0 +1,22 @@
+package com.upgrad.FoodOrderingApp.service.dao;
+
+import com.upgrad.FoodOrderingApp.service.entity.AddressEntity;
+import com.upgrad.FoodOrderingApp.service.entity.StateEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class StateDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    public List<StateEntity> getAllStates()
+    {
+        return null;
+    }
+}
